257473b77385647dcef541f627d0dadf91a11a79
Nde-Code <84632287+Nde-Code@users.noreply.github.com>
2026-03-24 08:44:37 +0000

    feat: adjust dashboard and authentication (incomplete).

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,8 +1,9 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = 'dashboard'
+
 urlpatterns = [
-    path('dashboard/', views.dashboard, name='dashboard'),
+    path('', views.dashboard, name='index'), 
     path('logout/', views.logout_view, name='logout'),
 ]
