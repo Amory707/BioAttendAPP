2841c0666934a15e71c9d89588b80b1dc86795c5
Nde-Code <84632287+Nde-Code@users.noreply.github.com>
2026-04-02 16:40:45 +0000

    fix(ci): pgvector was not found.

--- Employee/migrations/0001_initial.py
+++ Employee/migrations/0001_initial.py
@@ -11,6 +11,10 @@ class Migration(migrations.Migration):
     dependencies = []
 
     operations = [
+        migrations.RunSQL(
+            "CREATE EXTENSION IF NOT EXISTS vector;",
+            reverse_sql="DROP EXTENSION IF EXISTS vector;",
+        ),
         migrations.CreateModel(
             name="Employee",
             fields=[
